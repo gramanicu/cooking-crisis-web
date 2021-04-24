321bea0db9daf437332836dba8158d59008f75ef
Cristi Chifu <chifancristian99@gmail.com>
2021-04-24 17:00:35 +0300

    init frontend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>this is a message for patrick</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
