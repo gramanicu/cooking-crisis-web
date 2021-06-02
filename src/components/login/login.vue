fda524162a57f01b9e0909a669e4912b2f01614e
Grama Nicolae <gramanicu@gmail.com>
2021-06-02 12:39:04 +0300

    Added information checking to the login

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -8,9 +8,9 @@
             </div>
             <div class="infobox2" >
                 <h1 class="infoTitle fs-small-3"> Sign in</h1>
-                <v-text-field class="inputField" dark placeholder="Username"  v-model="usernameValue"></v-text-field>
-                <v-text-field class="inputField" dark placeholder="Password" type="password" v-model="passwordValue"></v-text-field>
-                <v-btn dark class="loginButton" :loading="load" @click="login()">login</v-btn>
+                <v-text-field class="inputField" dark placeholder="Username" @input="clearFail" :rules="[validUsername]" required v-model="usernameValue"></v-text-field>
+                <v-text-field class="inputField" dark placeholder="Password" @input="clearFail"  type="password" :rules="[validPassword]" required v-model="passwordValue"></v-text-field>
+                <v-btn dark class="loginButton" :loading="load" :disabled="p_invalid || u_invalid" @click="login()">login</v-btn>
                 <p class="signOutMessage">You dont have an account?</p>
                 <v-btn dark class="accountButton" @click="createAccount()" plain >create an account </v-btn>
                 <p class="fail" v-if="fail">Your username or password is wrong</p>
@@ -22,12 +22,58 @@
 
 <script>
 import {signIn} from '../../services/users_public'
+import {username_regexp, password_regexp} from '../../assets/constants/_constants'
+
 export default {
     created() {
     },
     mounted() {
     },
+    data() {
+        return {
+            load: false,
+            fail: false,
+            u_invalid: false,
+            p_invalid: false,
+            usernameValue: "",
+            passwordValue: ""
+        }
+    },
     methods: {
+        clearFail() {
+            this.fail = false
+        },
+        validUsername(input) {
+            if(input == "") {
+                this.u_invalid = true;
+                return "Username required";
+            }
+
+            const username_tester = new RegExp(username_regexp);
+
+            const valid = username_tester.test(input)
+            if(!valid) {
+                this.u_invalid = true;
+                return "Username not valid"
+            }
+            this.u_invalid = false;
+            return true
+        },
+        validPassword(input) {
+            if(input == "") {
+                this.p_invalid = true;
+                return "Password required";
+            }
+            const password_tester = new RegExp(password_regexp);
+
+            const valid = password_tester.test(input)
+            if(!valid) {
+                this.p_invalid = true;
+                return "Password not valid"
+            }
+            this.p_invalid = false;
+            return true
+        },
         async login() {
             this.load = true;
             this.fail = false;
